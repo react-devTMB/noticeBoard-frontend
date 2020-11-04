e910aefceb73d19005bf51a9327c9d3475f4f172
msko <msko@mcnc.co.kr>
2020-11-04 16:43:14 +0900

    Add signUp, login

--- notice-board/src/components/pages/LoginPages.js
+++ notice-board/src/components/pages/LoginPages.js
@@ -3,17 +3,33 @@ import { Button, Form, FormGroup, Input } from 'reactstrap';
 import { FacebookLoginButton, GoogleLoginButton, GithubLoginButton } from 'react-social-login-buttons';
 import styled from 'styled-components';
 import KakaoLogin from 'react-kakao-login';
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 import Title from '../common/Title';
 import KakaoImage from '../../assets/images/kakao_login_medium_wide.png';
 import { FACEBOOK_AUTH_URL, NAVER_AUTH_URL, GITHUB_AUTH_URL } from '../common/Constants';
+import axios from 'axios';
 
+const LoginPage = ({ history }) => {
+  const handleOnSubmit = async (e) => {
+    e.preventDefault();
 
-const LoginPage = () => {
+    await axios
+      .post('/auth/login', {
+        email: e.target.email.value,
+        password: e.target.password.value,
+      })
+      .then((res) => {
+        console.log('response', res);
+
+        // if (!data.email) {
+        //   history.push('/signUp');
+        // }
+      });
+  };
 
   return (
     <div className="login_wrap">
-      <Form className="login-form">
+      <Form className="login-form" onSubmit={handleOnSubmit}>
         <Title text="welcome to TMB~!!"></Title>
         <FormGroup className="mc_draw">
           <Input type="text" name="email" placeholder="Email" className="mc_checkmark"></Input>
@@ -21,14 +37,28 @@ const LoginPage = () => {
         <FormGroup>
           <Input type="password" name="password" placeholder="Password"></Input>
         </FormGroup>
-        <Button className="btn-lg btn-dark btn-block">Login</Button>
+        <Button type="submit" className="btn-lg btn-dark btn-block">
+          Login
+        </Button>
         <div className="text-center pt-3">-Or continue with your social account-</div>
-        <FacebookLoginButton onClick={() => window.open( FACEBOOK_AUTH_URL)}  className="mt-3 mb-3" style={{ fontSize: '15px' }} align="center" ></FacebookLoginButton>
-        <GoogleLoginButton  className="mt-3 mb-3" style={{ fontSize: '15px' }} align="center" />
-        <GithubLoginButton  onClick={() => window.open( GITHUB_AUTH_URL)} className="mt-3 mb-3" style={{ fontSize: '15px' }} align="center" />
+        <FacebookLoginButton
+          onClick={() => window.open(FACEBOOK_AUTH_URL)}
+          className="mt-3 mb-3"
+          style={{ fontSize: '15px' }}
+          align="center"
+        ></FacebookLoginButton>
+        <GoogleLoginButton className="mt-3 mb-3" style={{ fontSize: '15px' }} align="center" />
+        <GithubLoginButton
+          onClick={() => window.open(GITHUB_AUTH_URL)}
+          className="mt-3 mb-3"
+          style={{ fontSize: '15px' }}
+          align="center"
+        />
         <KakaoBtn />
-        
-        <a href={NAVER_AUTH_URL}><div className="btn_naver" ></div></a>
+
+        <a href={NAVER_AUTH_URL}>
+          <div className="btn_naver"></div>
+        </a>
         <div className="text-center">
           <Link to="/signUp">Sign up</Link>
           <span className="p-2">|</span>
@@ -39,13 +69,12 @@ const LoginPage = () => {
   );
 };
 
-
 const KakaoBtn = styled(KakaoLogin)`
   display: block;
   border: 0px;
   border-radius: 3px;
   box-shadow: rgba(0, 0, 0, 0.5) 0px 1px 2px;
-  color : #fff;
+  color: #fff;
   cursor: pointer;
   margin: 1rem 5px;
   width: calc(100% - 10px);
@@ -59,6 +88,5 @@ const KakaoBtn = styled(KakaoLogin)`
     box-shadow: 0 0px 15px 0 rgba(0, 0, 0, 0.2);
   }
 `;
-
 
 export default LoginPage;
--- notice-board/src/components/pages/SignUpPages.js
+++ notice-board/src/components/pages/SignUpPages.js
@@ -1,73 +1,83 @@
 import React, { useState } from 'react';
 import Title from '../common/Title';
-import { EMAIL_REG, PWD_REG } from '../common/Constants';
+import { EMAIL_REG, PWD_REG } from '../common/Constants';
 import { Button, Form, FormGroup, Input } from 'reactstrap';
+import axios from 'axios';
 
-const SignUpPages = () => {
+const SignUpPages = ({ history }) => {
+  const [errorTxt, setErrorTxt] = useState('');
 
-  const [ errorTxt, setErrorTxt] = useState('');
-
-  const [ registForm , setRegistForm ] = useState({
-    'nickName' : '',
-    'email' : '',
-    'password' : '',
-    'passwordConfirm' : ''
+  const [registForm, setRegistForm] = useState({
+    nickName: '',
+    email: '',
+    password: '',
+    passwordConfirm: '',
   });
 
   const validationCheck = (e) => {
+    const { name, value } = e.target;
 
-    const { name , value } = e.target;
-
-    console.log('registForm >> ' , registForm);
-    switch(e.target.name) {
-      case "nickName" :
-        if (value.length >0 && value.length <= 8) {
+    console.log('registForm >> ', registForm);
+    switch (e.target.name) {
+      case 'nickName':
+        if (value.length > 0 && value.length <= 8) {
           setErrorTxt('');
-          e.target.parentElement.classList.add("mc_checkmark");
+          e.target.parentElement.classList.add('mc_checkmark');
         } else {
-          setErrorTxt('닉네임은 8자 이하만 사용 가능합니다.')
-          e.target.parentElement.classList.remove("mc_checkmark");
+          setErrorTxt('닉네임은 8자 이하만 사용 가능합니다.');
+          e.target.parentElement.classList.remove('mc_checkmark');
         }
         break;
-        // TODO: Email 중복체크
-        case "email" :
-          if (EMAIL_REG.test(value)) {
-            setErrorTxt('');
-            e.target.parentElement.classList.add("mc_checkmark");
-          } else {
-            setErrorTxt('이메일 형식이 아닙니다.');
-            e.target.parentElement.classList.remove("mc_checkmark");
-          }
-          break;
-        case "password" :
-          if (value.length >= 8 && value.length <= 16 && PWD_REG.test(value)) {
-            setErrorTxt('');
-            e.target.parentElement.classList.add("mc_checkmark");
-          } else {
-            setErrorTxt('비밀번호는 8자이상 16자 이하, 영문, 숫자, 특수문자 조합이어야 합니다.');
-            e.target.parentElement.classList.remove("mc_checkmark");
-          }
-          break;
-        case "passwordConfirm" :
-          if (registForm.password === value) {
-            setErrorTxt('');
-            e.target.parentElement.classList.add("mc_checkmark");
-          } else {
-            setErrorTxt('비밀번호가 일치하지 않습니다.');
-            e.target.parentElement.classList.remove("mc_checkmark");
-          }
-          break;
-        default : break;
+      // TODO: Email 중복체크
+      case 'email':
+        if (EMAIL_REG.test(value)) {
+          setErrorTxt('');
+          e.target.parentElement.classList.add('mc_checkmark');
+        } else {
+          setErrorTxt('이메일 형식이 아닙니다.');
+          e.target.parentElement.classList.remove('mc_checkmark');
+        }
+        break;
+      case 'password':
+        if (value.length >= 8 && value.length <= 16 && PWD_REG.test(value)) {
+          setErrorTxt('');
+          e.target.parentElement.classList.add('mc_checkmark');
+        } else {
+          setErrorTxt('비밀번호는 8자이상 16자 이하, 영문, 숫자, 특수문자 조합이어야 합니다.');
+          e.target.parentElement.classList.remove('mc_checkmark');
+        }
+        break;
+      case 'passwordConfirm':
+        if (registForm.password === value) {
+          setErrorTxt('');
+          e.target.parentElement.classList.add('mc_checkmark');
+        } else {
+          setErrorTxt('비밀번호가 일치하지 않습니다.');
+          e.target.parentElement.classList.remove('mc_checkmark');
+        }
+        break;
+      default:
+        break;
     }
 
     setRegistForm({ ...registForm, [name]: value.trim() });
+  };
 
-  }
-
-  const handleOnSubmit = e => {
+  const handleOnSubmit = async (e) => {
     e.preventDefault();
-    console.log("registForm >> " , registForm);
-  }
+    console.log('registForm >> ', registForm);
+
+    await axios
+      .post('/auth/register', {
+        name: registForm.nickName,
+        email: registForm.email,
+        password: registForm.password,
+        passwordConfirm: registForm.passwordConfirm,
+      })
+      .then((res) => {
+        console.log('response', res);
+      });
+  };
 
   return (
     <div className="login_wrap">
@@ -79,26 +89,20 @@ const SignUpPages = () => {
             type="text"
             name="nickName"
             placeholder="NickName"
-            onChange={ validationCheck }
-            value={ registForm.nickName }
+            onChange={validationCheck}
+            value={registForm.nickName}
           />
         </FormGroup>
         <FormGroup>
-          <Input
-            type="email"
-            name="email"
-            placeholder="Email"
-            onChange={ validationCheck }
-            value={ registForm.email }
-          />
+          <Input type="email" name="email" placeholder="Email" onChange={validationCheck} value={registForm.email} />
         </FormGroup>
         <FormGroup>
           <Input
             type="password"
             name="password"
             placeholder="Password"
-            onChange={ validationCheck }
-            value={ registForm.password }
+            onChange={validationCheck}
+            value={registForm.password}
           />
         </FormGroup>
         <FormGroup className="form-group_02">
@@ -106,15 +110,17 @@ const SignUpPages = () => {
             type="password"
             name="passwordConfirm"
             placeholder="PasswordConfirm"
-            onChange={ validationCheck }
-            value={ registForm.passwordConfirm }
+            onChange={validationCheck}
+            value={registForm.passwordConfirm}
           />
         </FormGroup>
-        <p className="chk_validate">{ errorTxt }</p>
-        <Button disabled={ errorTxt !== ''} className="btn-lg btn-dark btn-block" onClick={ handleOnSubmit }>Sign Up</Button>
+        <p className="chk_validate">{errorTxt}</p>
+        <Button disabled={errorTxt !== ''} className="btn-lg btn-dark btn-block" onClick={handleOnSubmit}>
+          Sign Up
+        </Button>
       </Form>
     </div>
   );
 };
 
-  export default SignUpPages;
+export default SignUpPages;
